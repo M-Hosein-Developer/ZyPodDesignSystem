3446e32430f92f61ab85ea700c1c8ac1efc44ee5
m.hajiakbari <m.hosein.developer@gmail.com>
2025-01-21 16:22:19 +0330

    Setup button design system

---
+++ app/src/main/java/com/example/zypoddesignsystem/DataRepository.kt
@@ -0,0 +1,33 @@
+package com.example.zypoddesignsystem
+
+import android.text.Layout
+
+class DataRepository {
+
+    fun getViewById(id : Int) : Int =
+        when(id){
+            1 -> R.layout.button_description_layout
+            2 -> R.layout.activity_detail
+            3 -> R.layout.activity_detail
+            4 -> R.layout.activity_detail
+            5 -> R.layout.activity_detail
+            6 -> R.layout.activity_detail
+            7 -> R.layout.activity_detail
+            8 -> R.layout.activity_detail
+            9 -> R.layout.activity_detail
+            10 -> R.layout.activity_detail
+            11 -> R.layout.activity_detail
+            12 -> R.layout.activity_detail
+            13 -> R.layout.activity_detail
+            14 -> R.layout.activity_detail
+            15 -> R.layout.activity_detail
+            16 -> R.layout.activity_detail
+            17 -> R.layout.activity_detail
+            18 -> R.layout.activity_detail
+            19 -> R.layout.activity_detail
+            20 -> R.layout.activity_detail
+            21 -> R.layout.activity_detail
+            else -> R.layout.activity_detail
+        }
+
+}
--- app/src/main/java/com/example/zypoddesignsystem/DetailActivity.kt
+++ app/src/main/java/com/example/zypoddesignsystem/DetailActivity.kt
@@ -3,6 +3,7 @@ package com.example.zypoddesignsystem
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,7 +12,7 @@ import com.example.zypoddesignsystem.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityDetailBinding
+    private var id : Int = 0
 
     companion object{
         fun showDetail(context: Context , id : Int){
@@ -26,12 +27,22 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_detail)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        handleIntent()
+
+        setContentView(DataRepository().getViewById(id))
     }
+
+    private fun handleIntent(){
+        intent?.let {
+            id = it.getIntExtra(DETAIL_ACTIVITY , 0)
+        }
+    }
+
 }
