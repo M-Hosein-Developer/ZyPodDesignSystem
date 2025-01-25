3b1a042dea815bdc340f232490da970cffb0bbbf
m.hajiakbari <m.hosein.developer@gmail.com>
2025-01-25 14:04:48 +0330

    setup dropdown design system

--- app/src/main/java/com/example/zypoddesignsystem/DetailActivity.kt
+++ app/src/main/java/com/example/zypoddesignsystem/DetailActivity.kt
@@ -3,12 +3,14 @@ package com.example.zypoddesignsystem
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.zypoddesignsystem.databinding.ActivityDetailBinding
+import com.example.zypoddesignsystem.databinding.InputDetailLayoutBinding
 
 class DetailActivity : AppCompatActivity() {
 
@@ -33,6 +35,12 @@ class DetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+//        val items = listOf("انتخاب کنید", "گزینه 1", "گزینه 2", "گزینه 3", "گزینه 4")
+//
+//        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, items)
+//        binding.autoCompleteTextView.setAdapter(adapter)
 
         handleIntent()
 
